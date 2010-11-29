0fbde92702d6c433613145d043bf6ee609cdb1bb
arfrank <arfrank@gmail.com>
2010-11-28 20:03:49 -0500

    Adding sql file the correct way

--- sql-updates/43.sql
+++ sql-updates/43.sql
@@ -1,3 +1,3 @@
-INSERT INTO settings SET name='validate_twiml_requests', value='1', tenant_id=1;
+INSERT INTO settings (tenant_id,name,value) VALUES (1,'validate_twiml_requests','');
 
 UPDATE `settings` SET `value` = 43 WHERE `name` = 'schema-version';
